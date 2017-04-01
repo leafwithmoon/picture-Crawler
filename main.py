e0cab6d58a1e8df8f17fec77b680fea9120131c4
zhangkeyf <zhangkeyf@oneapm.com>
2017-04-01 18:57:43 +0800

    add main function to get the picture

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ from fetchPage import fetch
 
 init_html_link = 'http://www.mzitu.com/all'
 html_link = 'http://www.mzitu.com/page'
-html_doc = fetch('http://www.mzitu.com/all')
+
+html_doc = fetch(init_html_link)
 soup = BeautifulSoup(html_doc, "lxml")
 
-max_pageNumber = soup.find_all(class_='page-numbers')[5].text
+max_pageNumber = int(soup.find_all(class_='page-numbers')[5].text)
+
+# for pageNumber in range(1, max_pageNumber):
+#     html_doc = fetch(html_link + pageNumber)
+#     soup = BeautifulSoup(html_doc, "lxml")
+
+html_doc = fetch(html_link + "2")
+soup = BeautifulSoup(html_doc, "lxml")
+print soup.prettify()
