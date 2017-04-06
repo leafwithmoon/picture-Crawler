f3ff66553493d7facc45ba14fdc59ea72221adfa
zhangkeyf <zhangkeyf@oneapm.com>
2017-04-06 17:00:55 +0800

    Finish the main function

--- fetchPage.py
+++ fetchPage.py
@@ -1,5 +1,6 @@
 import requests
 
+headers = {'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1"}
 def fetch(url):
-    res = requests.get(url=url)
-    return res.text
+    res = requests.get(url=url, headers=headers)
+    return res
--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 # coding = utf-8
 from bs4 import BeautifulSoup
 from fetchPage import fetch
+import os
 
-init_html_link = 'http://www.mzitu.com/all'
-html_link = 'http://www.mzitu.com/page'
+save_location = "D:\meizitu\\"
+init_html_link = 'http://www.mzitu.com'
+html_link = 'http://www.mzitu.com/page/'
 
-html_doc = fetch(init_html_link)
-soup = BeautifulSoup(html_doc, "lxml")
+init_html_doc = fetch(init_html_link).text
+soup = BeautifulSoup(init_html_doc, "lxml")
 
 max_pageNumber = int(soup.find_all(class_='page-numbers')[5].text)
-
-# for pageNumber in range(1, max_pageNumber):
-#     html_doc = fetch(html_link + pageNumber)
-#     soup = BeautifulSoup(html_doc, "lxml")
-
-html_doc = fetch(html_link + "2")
-soup = BeautifulSoup(html_doc, "lxml")
-print soup.prettify()
+print "Max PageNumber is {0}".format(max_pageNumber)
+for pageNumber in range(1, max_pageNumber+1):
+    print "In page {0}".format(pageNumber)
+    html_doc = fetch(html_link + str(pageNumber)).text
+    soup = BeautifulSoup(html_doc, "lxml")
+    cover = soup.find_all(class_='lazy')
+    for x in cover:
+        cover_name = x.get('alt').replace(' ', '').replace(',', '').replace('?', '')
+        if not(os._exists(save_location + cover_name)):
+            print cover_name
+            cover_url = x.parent.get('href')
+            cover_html_doc = fetch(cover_url).text
+            cover_soup = BeautifulSoup(cover_html_doc, "lxml")
+            cover_max_pageNumber = int(cover_soup.find(class_='pagenavi').find_all()[-3].text)
+            os.makedirs(os.path.join(save_location, cover_name))
+            os.chdir(save_location + cover_name)
+            for cover_pageNumber in range(1, cover_max_pageNumber):
+                picture_html_doc = fetch(cover_url+'/'+str(cover_pageNumber)).text
+                picture_soup = BeautifulSoup(picture_html_doc, "lxml")
+                picture = picture_soup.find('img')
+                picture_href = picture.get('src')
+                img = fetch(picture_href)
+                f = open(str(cover_pageNumber) + '.jpg', 'ab')
+                f.write(img.content)
+                f.close()
